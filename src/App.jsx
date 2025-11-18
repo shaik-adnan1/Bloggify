d0e7aa9385682727a64d5948ee678e90fce89791
sanass3258 <137000579+sfamily788@users.noreply.github.com>
2025-11-18 13:23:43 -0600

    Implemented blog card grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const blogs = [
+  {
+    image: "/mountain.jpg",
+    author: "Alec Whitten • 1 Jan 2023",
+    title: "Bill Walsh leadership lessons",
+    desc: "Like to know the secrets of transforming a 2–14 team into a 3x Super Bowl winning dynasty?",
+    tags: ["Leadership", "Management"]
+  },
+  {
+    image: "/meeting.jpg",
+    author: "Demi Wilkinson • 1 Jan 2023",
+    title: "PM mental models",
+    desc: "Mental models are simple expressions of complex processes or relationships.",
+    tags: ["Product", "Research", "Frameworks"]
+  },
+  {
+    image: "/desk.jpg",
+    author: "Candice Wu • 1 Jan 2023",
+    title: "What is Wireframing?",
+    desc: "Intro to wireframing and its principles. Learn from industry experts.",
+    tags: ["Design", "Research"]
+  },
+  {
+    image: "/designer.jpg",
+    author: "Natali Craig • 1 Jan 2023",
+    title: "How collaboration makes us better designers",
+    desc: "Collaboration can make our teams stronger and our designs better.",
+    tags: ["Design", "Research"]
+  },
+  {
+    image: "/codeguy.jpg",
+    author: "Drew Cano • 1 Jan 2023",
+    title: "Our top 10 JS frameworks to use",
+    desc: "JavaScript frameworks make development easy with extensive features.",
+    tags: ["Software Development", "Tools", "SaaS"]
+  },
+  {
+    image: "/podcast.jpg",
+    author: "Orlando Diggs • 1 Jan 2023",
+    title: "Podcast: Creating a better CX community",
+    desc: "Starting a community doesn’t need to be complicated.",
+    tags: ["Podcasts", "Customer Success"]
+  }
+];
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="container">
+      <h1>All blog posts</h1>
 
-export default App
+      <div className="grid">
+        {blogs.map((blog, i) => (
+          <div className="card" key={i}>
+            <img src={blog.image} alt="" />
+
+            <div className="card-content">
+              <div className="author">{blog.author}</div>
+              <div className="title">{blog.title}</div>
+              <div className="desc">{blog.desc}</div>
+
+              <div className="tags">
+                {blog.tags.map((tag, index) => (
+                  <span key={index}>{tag}</span>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
